bed8a27d346ba7b39265e4ab2c29010a40aa87a3
CelineSilingSong <ss4799@princeton.edu>
2024-07-10 16:30:46 -0400

    should have changed initialization

--- MatchingClassified.py
+++ MatchingClassified.py
@@ -4,11 +4,12 @@ import pandas as pd
 from Matcher import FuzzyMatcher
 from Matcher import CodeMatcher
 
-classified_df = 'national_classified_sheet.xlsx'
-df_2022 = 'national_M2022_dl.xlsx'
+classified_df = pd.read_excel('national_classifed_sheet.xlsx')
+df_2022 = pd.read_excel('national_M2022_dl.xlsx')
 
-fuzzymatcher = FuzzyMatcher(classified_df, df_2022,1,1,0)
+fuzzymatcher = FuzzyMatcher()
 
-df_matched_list = fuzzymatcher.return_matched_list()
+df_matched_list = fuzzymatcher.return_matched_list(classified_df[:,1], df_2022[:,1])
 
+df_matched_list['Code'] = df_matched_list.apply(lambda row: fuzzymatcher.get_code(row, 4, df_2022[:,0]))
 df_matched_list.to_csv('matched list')
